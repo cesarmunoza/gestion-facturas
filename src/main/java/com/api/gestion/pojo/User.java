15f0c385033a799d095ff47d655cf497c2c86a04
Cesar Munoz <cesarmu@hotmail.com>
2024-10-30 16:16:24 -0500

    Se crearon los packages de constantes, dao, jwt, pojo, rest, service, util y wraper, se creó la clase pojo User, el UserDao, UserService y la implementación, La clase de utilidades FacturaUtils, y las constantes para usar en la aplicación

---
+++ src/main/java/com/api/gestion/pojo/User.java
@@ -0,0 +1,43 @@
+package com.api.gestion.pojo;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@NamedQuery(name = "User.findByEmail", query = "select u from User where u.email=:email")
+@Data
+@Entity
+@DynamicUpdate
+@DynamicInsert
+@Table(name = "users")
+public class User implements Serializable {
+
+
+    @Serial
+    private static final long serialVersionUID = 1482168378210410822L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "numero_de_contacto")
+    private String numeroDeContacto;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "role")
+    private String role;
+}
---
+++ src/main/java/com/api/gestion/service/impl/UserServiceImpl.java
@@ -0,0 +1,69 @@
+package com.api.gestion.service.impl;
+
+import com.api.gestion.constantes.FacturaConstantes;
+import com.api.gestion.dao.UserDao;
+import com.api.gestion.pojo.User;
+import com.api.gestion.service.UserService;
+import com.api.gestion.util.FacturaUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.Objects;
+
+@Slf4j
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserDao userDao;
+
+    public UserServiceImpl(UserDao userDao){
+        this.userDao = userDao;
+    }
+
+    @Override
+    public ResponseEntity<String> signUp(Map<String, String> requestMap) {
+        log.info("Registro interno de un usuario {}",requestMap);
+        try {
+            if (validateSignUpMap(requestMap)){
+                User user = userDao.findByEmail(requestMap.get("email"));
+                if (Objects.isNull(user)){
+                    userDao.save(getUserFromMap(requestMap));
+                    return FacturaUtils.getResponseEntity("Usuario registrado con éxito", HttpStatus.CREATED);
+                }else {
+                    return FacturaUtils.getResponseEntity("El usuario con ese email, ya existe", HttpStatus.BAD_REQUEST);
+                }
+            }else {
+                return FacturaUtils.getResponseEntity(FacturaConstantes.INVALID_DATA, HttpStatus.BAD_REQUEST);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return FacturaUtils.getResponseEntity(FacturaConstantes.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    private boolean validateSignUpMap(Map<String, String> requestMap){
+        if (requestMap.containsKey("nombre")
+        && requestMap.containsKey("numeroContacto")
+        && requestMap.containsKey("email")
+        && requestMap.containsKey("password")){
+            return true;
+        }
+        return false;
+    }
+
+    private User getUserFromMap(Map<String, String> requestMap){
+        User user = new User();
+        user.setNombre(requestMap.get("nombre"));
+        user.setNumeroDeContacto(requestMap.get("numeroContacto"));
+        user.setEmail(requestMap.get("email"));
+        user.setPassword(requestMap.get("password"));
+        user.setStatus(requestMap.get("false"));
+        user.setRole(requestMap.get("user"));
+        return user;
+    }
+}
